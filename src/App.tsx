94297f2d4a962efd3ad9dbd6a95a647e784d3661
alexc <alexccarpal88@gmail.com>
2023-03-30 16:54:05 -0400

    add some components and basic functionalities

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,41 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React, { useEffect, useState } from "react";
+import TaskList from "./TaskList";
+import { tasksStore } from "./sampleTasks";
+
+interface TaskType {
+  id: number;
+  name: string;
+  description: string;
+  dueDate: string;
+  completed: boolean;
+}
 
 function App() {
+  const [tasks, setTasks] = useState<TaskType[]>([]);
+
+  const loadTasks = useEffect(() => {
+    setTasks(tasksStore);
+  }, []);
+  const handleSaveTask = (task: TaskType) => {
+    setTasks([...tasks, task]);
+  };
+  const handleEditTask = (taskId: number) => {
+    // setTasks([...tasks, task]);
+  };
+  const handleDeleteTask = (taskId: number) => {
+    const newTasks = tasks.filter((task: TaskType) => {
+      return task.id !== taskId;
+    });
+    setTasks(newTasks);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> Hello World.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <TaskList
+        tasks={tasks}
+        onDeleteTask={handleDeleteTask}
+        onEditTask={handleEditTask}
+      />
     </div>
   );
 }
---
+++ src/sampleTasks.ts
@@ -0,0 +1,30 @@
+export const tasksStore = [
+  {
+    id: 1,
+    name: "Abrir puerta",
+    description: "El clásico movimiento de abrir la puerta una vez al dia",
+    dueDate: "1680208432291",
+    completed: false,
+  },
+  {
+    id: 2,
+    name: "Comer abanico",
+    description: "Nada mejor que la ingesta de una vez al dia",
+    dueDate: "2021021423450",
+    completed: false,
+  },
+  {
+    id: 3,
+    name: "Jugar futbol",
+    description: "El clásico barsa madrid una vez mas",
+    dueDate: "2021021423435",
+    completed: false,
+  },
+  {
+    id: 4,
+    name: "Terminar el plotter",
+    description: "Sin ignorar la velocidad de la luz y la corriente",
+    dueDate: "2021021423345",
+    completed: false,
+  },
+];
